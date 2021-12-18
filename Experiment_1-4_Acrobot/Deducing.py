8d67af062abc36f68c4f0da8c18d8bb06452f89e
DeepDeducing <60392437+DeepDeducing@users.noreply.github.com>
2021-12-18 23:00:07 +0800

    Update Deducing.py

--- Experiment_1-4_Acrobot/Deducing.py
+++ Experiment_1-4_Acrobot/Deducing.py
@@ -33,7 +33,7 @@ for trials in range(100):                                                  # <<<
 
     from Brain_for_deducing import *                                           # <<<<<<<<<<<<
     network_size           = np.array([100 * 6 + 3 * 100, 100, 100, 100, 100]) # <<<<<<<<<<<<
-    beta                   = 1                                                 # <<<<<<<<<<<<
+    beta                   = 0.1                                               # <<<<<<<<<<<<
     epoch_of_deducing      = 100000                                            # <<<<<<<<<<<<
     drop_rate              = 0.75                                              # <<<<<<<<<<<<
     Machine                = Brain(network_size, beta, epoch_of_deducing, drop_rate)
