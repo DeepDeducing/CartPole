9f728e0aed415d0d1bde4be5cadf4c3b1af4fa04
DeepDeducing <60392437+DeepDeducing@users.noreply.github.com>
2022-01-03 17:53:32 +0800

    Update Deducing.py

--- Experiment_1-1_CartPole/Deducing.py
+++ Experiment_1-1_CartPole/Deducing.py
@@ -34,7 +34,7 @@ for trials in range(100):                                                  # <<<
     from Brain_for_deducing import *                                       # <<<<<<<<<<<<
     network_size           = np.array([4*100 + 2 * 25, 100, 100, 100, 64]) # <<<<<<<<<<<<
     beta                   = 0.1                                           # <<<<<<<<<<<<
-    epoch_of_deducing      = 1000                                          # <<<<<<<<<<<<
+    epoch_of_deducing      = 2000                                          # <<<<<<<<<<<<
     drop_rate              = 0.75                                          # <<<<<<<<<<<<
     Machine                = Brain(network_size, beta, epoch_of_deducing, drop_rate)
 
